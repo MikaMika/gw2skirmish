5857d6795bd33d897701a93efa463c54ce0df241
Michaël Dima <maniacmika@gmail.com>
2023-04-01 14:17:02 +0000

    Untangle spaghetti

--- make_html.sh
+++ make_html.sh
@@ -68,220 +68,188 @@ make_list_worlds() {
 }
 
 make_match() {
-    echo "<div id='results'>"
-    echo "<h2>results</h2>"
-    MATCHES=$(jq ".|keys[]" matches.json)
-    for match in $MATCHES; do
-        echo "<div class='match' style='margin-bottom: 300px;'>"
-        match_id=$(jq -r ".[$match].id" matches.json)
-        echo "<h3 id='$match_id'>$match_id</h3>"
-        
-        vp_red=$(jq ".[$match].victory_points.red" matches.json)
-        vp_blue=$(jq ".[$match].victory_points.blue" matches.json)
-        vp_green=$(jq ".[$match].victory_points.green" matches.json)
-        SKIRMISH_TOTAL=84
-        skirmish_done=$(( (vp_red+vp_blue+vp_green) / (3+4+5) ))
-        skirmish_remaining=$((SKIRMISH_TOTAL - skirmish_done))
-        vp_remaining=$((skirmish_remaining * 2))
-        echo "<p>Skirmish: $skirmish_done/$SKIRMISH_TOTAL ($skirmish_remaining left, max $vp_remaining VPd)</p>"
-        
-        vp_max=$((skirmish_done * 5))
-        vp_min=$((skirmish_done * 3))
-        
-        
-        if [ "$vp_red" -gt "$vp_blue" -a "$vp_red" -gt "$vp_green" ]
-        then
-          frst=$vp_red
-          frst_color="red"
-          if [ "$vp_blue" -gt "$vp_green" ]
-          then
-            scnd=$vp_blue
-            scnd_color="blue"
-            thrd=$vp_green
-            thrd_color="green"
-          else
-            scnd=$vp_green
-            scnd_color="green"
-            thrd=$vp_blue
-            thrd_color="blue"
-          fi
-        elif [ "$vp_blue" -gt "$vp_red" -a "$vp_blue" -gt "$vp_green" ]
-        then
-          frst=$vp_blue
-          frst_color="blue"
-          if [ "$vp_red" -gt "$vp_green" ]
-          then
-            scnd=$vp_red
-            scnd_color="red"
-            thrd=$vp_green
-            thrd_color="green"
-          else
-            scnd=$vp_green
-            scnd_color="green"
-            thrd=$vp_red
-            thrd_color="red"
-          fi
-        else
-          frst=$vp_green
-          frst_color="green"
-          if [ "$vp_red" -gt "$vp_blue" ]
-          then
-            scnd=$vp_red
-            scnd_color="red"
-            thrd=$vp_blue
-            thrd_color="blue"
-          else
-            scnd=$vp_blue
-            scnd_color="blue"
-            thrd=$vp_red
-            thrd_color="red"
-          fi
-        fi
-        
-        
-        [ $vp_max = $vp_min ] && frst_victory_ratio=0 || frst_victory_ratio=$(( 100 * (frst - vp_min) / (vp_max - vp_min) ))
-        frst_vp_difference=$((frst - scnd))
-        frst_tie=$(( (vp_remaining - frst_vp_difference) / 2 ))
-        frst_secure=$((frst_tie + 1))
-        frst_difficulty=$((100 * frst_secure / vp_remaining))
+  echo "<div id='results'>"
+  echo "<h2>results</h2>"
+  MATCHES=$(jq ".|keys[]" matches.json)
+  for match in $MATCHES
+  do
+    echo "<div class='match' style='margin-bottom: 300px;'>"
+    match_id=$(jq -r ".[$match].id" matches.json)
+    echo "<h3 id='$match_id'>$match_id</h3>"
+
+    vp_red=$(jq ".[$match].victory_points.red" matches.json)
+    vp_blue=$(jq ".[$match].victory_points.blue" matches.json)
+    vp_green=$(jq ".[$match].victory_points.green" matches.json)
+    SKIRMISH_TOTAL=84
+    skirmish_done=$(( (vp_red+vp_blue+vp_green) / (3+4+5) ))
+    skirmish_remaining=$((SKIRMISH_TOTAL - skirmish_done))
+    vp_remaining=$((skirmish_remaining * 2))
+    echo "<p>Skirmish: $skirmish_done/$SKIRMISH_TOTAL ($skirmish_remaining left, max $vp_remaining VPd)</p>"
+
+    vp_max=$((skirmish_done * 5))
+    vp_min=$((skirmish_done * 3))
 
 
-            echo "<p>"
-            for world_id in $(jq ".[$match].all_worlds.${frst_color}[]" matches.json); do
-                world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
-                world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
-                echo "<span class='$frst_color' style='color:$frst_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
-            done
-            echo "</p>"
-            kills=$(jq ".[$match].kills.$frst_color" matches.json)
-            deaths=$(jq ".[$match].deaths.$frst_color" matches.json)
-            [ $deaths = 0 ] && $deaths=1    # to avoid zero div
-            kdr=$(( kills * 100 / deaths ))
-            if [ "$kdr" -le 100 ]
-            then
-                percent=$(( kdr / 2 ))
-            else
-                percent=$(( 100 - 10000/(kdr*2) ))
-            fi
-            if [ "$percent" -gt 50 ]
-            then
-                lime=$percent && maroon=100
-            else
-                lime=0 && maroon=$(( percent*2 ))
-            fi
-            echo "<div class='green' style='display:inline-block; border:solid black 2px; background-color: green; height: 10px; width: 100px;'>
-<div class='lime' style='background-color: lime; width: ${lime}%; height: 100%;'>
-<div class='red' style='background-color: red; width: 50px; height: 100%;'>
-<div class='maroon' style='background-color: maroon; width: ${maroon}%; height: 100%;'>
-</div>
-</div>
-</div>
-</div> $kills kills / $deaths deaths ($(echo "scale=2; ($kdr/100)" | bc -l))"
-
-        echo "<p>:$frst_color: Victory Ratio: $frst_victory_ratio%<br>"
-        echo "$frst Victory Points<br>"
-        echo "VP difference with $scnd_color: $frst_vp_difference<br>"
-        echo "Will require +$frst_tie VP over $scnd_color for a tie, and one more to secure position<br>"
-        echo "Difficulty: $frst_difficulty%</p>"
-
-        [ $vp_max = $vp_min ] && scnd_victory_ratio=0 || scnd_victory_ratio=$(( 100 * (scnd - vp_min) / (vp_max - vp_min) ))
-        scnd_vp_difference=$((scnd - thrd))
-        scnd_tie=$(( (vp_remaining - scnd_vp_difference) / 2 ))
-        scnd_secure=$((scnd_tie + 1))
-        scnd_difficulty=$((100 * scnd_secure / vp_remaining))
-
-
-            echo "<p>"
-            for world_id in $(jq ".[$match].all_worlds.${scnd_color}[]" matches.json); do
-                world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
-                world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
-                echo "<span class='$scnd_color' style='color:$scnd_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
-            done
-            echo "</p>"
-            kills=$(jq ".[$match].kills.$scnd_color" matches.json)
-            deaths=$(jq ".[$match].deaths.$scnd_color" matches.json)
-            [ $deaths = 0 ] && $deaths=1    # to avoid zero div
-            kdr=$(( kills * 100 / deaths ))
-            if [ "$kdr" -le 100 ]
-            then
-                percent=$(( kdr / 2 ))
-            else
-                percent=$(( 100 - 10000/(kdr*2) ))
-            fi
-            if [ "$percent" -gt 50 ]
-            then
-                lime=$percent && maroon=100
-            else
-                lime=0 && maroon=$(( percent*2 ))
-            fi
-            echo "<div class='green' style='display:inline-block; border:solid black 2px; background-color: green; height: 10px; width: 100px;'>
-<div class='lime' style='background-color: lime; width: ${lime}%; height: 100%;'>
-<div class='red' style='background-color: red; width: 50px; height: 100%;'>
-<div class='maroon' style='background-color: maroon; width: ${maroon}%; height: 100%;'>
-</div>
-</div>
-</div>
-</div> $kills kills / $deaths deaths ($(echo "scale=2; ($kdr/100)" | bc -l))"
+    if [ "$vp_red" -gt "$vp_blue" ] && [ "$vp_red" -gt "$vp_green" ]
+    then
+      frst=$vp_red
+      frst_color="red"
+      if [ "$vp_blue" -gt "$vp_green" ]
+      then
+        scnd=$vp_blue
+        scnd_color="blue"
+        thrd=$vp_green
+        thrd_color="green"
+      else
+        scnd=$vp_green
+        scnd_color="green"
+        thrd=$vp_blue
+        thrd_color="blue"
+      fi
+    elif [ "$vp_blue" -gt "$vp_red" ] && [ "$vp_blue" -gt "$vp_green" ]
+    then
+      frst=$vp_blue
+      frst_color="blue"
+      if [ "$vp_red" -gt "$vp_green" ]
+      then
+        scnd=$vp_red
+        scnd_color="red"
+        thrd=$vp_green
+        thrd_color="green"
+      else
+        scnd=$vp_green
+        scnd_color="green"
+        thrd=$vp_red
+        thrd_color="red"
+      fi
+    else
+      frst=$vp_green
+      frst_color="green"
+      if [ "$vp_red" -gt "$vp_blue" ]
+      then
+        scnd=$vp_red
+        scnd_color="red"
+        thrd=$vp_blue
+        thrd_color="blue"
+      else
+        scnd=$vp_blue
+        scnd_color="blue"
+        thrd=$vp_red
+        thrd_color="red"
+      fi
+    fi
 
 
-        echo "<p>:$scnd_color: Victory Ratio: $scnd_victory_ratio%<br>"
-        echo "$scnd Victory Points<br>"
-        echo "VP difference with $thrd_color: $scnd_vp_difference<br>"
-        echo "Will require +$scnd_tie VP over opponent for a tie, and one more to secure position<br>"
-        echo "Difficulty: $scnd_difficulty%</p>"
+    [ $vp_max = $vp_min ] && frst_victory_ratio=0 \
+    || frst_victory_ratio=$(( 100 * (frst - vp_min) / (vp_max - vp_min) ))
 
-        [ $vp_max = $vp_min ] && thrd_victory_ratio=0 || thrd_victory_ratio=$(( 100 * (thrd - vp_min) / (vp_max - vp_min) ))
-        thrd_vp_difference=$((thrd - frst))
-        thrd_tie=$(( (vp_remaining - thrd_vp_difference) / 2 ))
-        thrd_secure=$((thrd_tie + 1))
-        thrd_difficulty=$((100 * thrd_secure / vp_remaining))
+    frst_vp_difference=$((frst - scnd))
+    frst_tie=$(( (vp_remaining - frst_vp_difference) / 2 ))
+    frst_secure=$((frst_tie + 1))
+    frst_difficulty=$((100 * frst_secure / vp_remaining))
 
 
-
-            echo "<p>"
-            for world_id in $(jq ".[$match].all_worlds.${thrd_color}[]" matches.json); do
-                world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
-                world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
-                echo "<span class='$thrd_color' style='color:$thrd_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
-            done
-            echo "</p>"
-            kills=$(jq ".[$match].kills.$thrd_color" matches.json)
-            deaths=$(jq ".[$match].deaths.$thrd_color" matches.json)
-            [ $deaths = 0 ] && $deaths=1    # to avoid zero div
-            kdr=$(( kills * 100 / deaths ))
-            if [ "$kdr" -le 100 ]
-            then
-                percent=$(( kdr / 2 ))
-            else
-                percent=$(( 100 - 10000/(kdr*2) ))
-            fi
-            if [ "$percent" -gt 50 ]
-            then
-                lime=$percent && maroon=100
-            else
-                lime=0 && maroon=$(( percent*2 ))
-            fi
-            echo "<div class='green' style='display:inline-block; border:solid black 2px; background-color: green; height: 10px; width: 100px;'>
-<div class='lime' style='background-color: lime; width: ${lime}%; height: 100%;'>
-<div class='red' style='background-color: red; width: 50px; height: 100%;'>
-<div class='maroon' style='background-color: maroon; width: ${maroon}%; height: 100%;'>
-</div>
-</div>
-</div>
-</div> $kills kills / $deaths deaths ($(echo "scale=2; ($kdr/100)" | bc -l))"
-
-
-        echo "<p>:$thrd_color: Victory Ratio: $thrd_victory_ratio%<br>"
-        echo "$thrd Victory Points<br>"
-        echo "VP difference with $frst_color: $thrd_vp_difference<br>"
-        echo "Will require +$thrd_tie VP to catch up with $frst_color<br>"
-        echo "Difficulty: $thrd_difficulty%</p>"
-        
-        
-        echo "<p><a href='#'>⬆️</a></p>"
-        echo "</div>"
+    echo "<p>"
+    for world_id in $(jq ".[$match].all_worlds.${frst_color}[]" matches.json)
+    do
+      world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
+      world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
+      echo "<span class='$frst_color' style='color:$frst_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
     done
+    echo "</p>"
+
+    kdr_bar "$frst_color"
+
+    echo "<p>:$frst_color: Victory Ratio: $frst_victory_ratio%<br>"
+    echo "$frst Victory Points<br>"
+    echo "VP difference with $scnd_color: $frst_vp_difference<br>"
+    echo "Will require +$frst_tie VP over $scnd_color for a tie, and one more to secure position<br>"
+    echo "Difficulty: $frst_difficulty%</p>"
+
+    [ $vp_max = $vp_min ] && scnd_victory_ratio=0 || scnd_victory_ratio=$(( 100 * (scnd - vp_min) / (vp_max - vp_min) ))
+    scnd_vp_difference=$((scnd - thrd))
+    scnd_tie=$(( (vp_remaining - scnd_vp_difference) / 2 ))
+    scnd_secure=$((scnd_tie + 1))
+    scnd_difficulty=$((100 * scnd_secure / vp_remaining))
+
+
+    echo "<p>"
+    for world_id in $(jq ".[$match].all_worlds.${scnd_color}[]" matches.json)
+    do
+      world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
+      world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
+      echo "<span class='$scnd_color' style='color:$scnd_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
+    done
+    echo "</p>"
+
+    kdr_bar "$scnd_color"
+
+    echo "<p>:$scnd_color: Victory Ratio: $scnd_victory_ratio%<br>"
+    echo "$scnd Victory Points<br>"
+    echo "VP difference with $thrd_color: $scnd_vp_difference<br>"
+    echo "Will require +$scnd_tie VP over opponent for a tie, and one more to secure position<br>"
+    echo "Difficulty: $scnd_difficulty%</p>"
+
+    [ $vp_max = $vp_min ] && thrd_victory_ratio=0 || thrd_victory_ratio=$(( 100 * (thrd - vp_min) / (vp_max - vp_min) ))
+    thrd_vp_difference=$((thrd - frst))
+    thrd_tie=$(( (vp_remaining - thrd_vp_difference) / 2 ))
+    thrd_secure=$((thrd_tie + 1))
+    thrd_difficulty=$((100 * thrd_secure / vp_remaining))
+
+    echo "<p>"
+    for world_id in $(jq ".[$match].all_worlds.${thrd_color}[]" matches.json)
+    do
+      world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
+      world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
+      echo "<span class='$thrd_color' style='color:$thrd_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
+    done
+    echo "</p>"
+
+    kdr_bar "$thrd_color"
+
+    echo "<p>:$thrd_color: Victory Ratio: $thrd_victory_ratio%<br>"
+    echo "$thrd Victory Points<br>"
+    echo "VP difference with $frst_color: $thrd_vp_difference<br>"
+    echo "Will require +$thrd_tie VP to catch up with $frst_color<br>"
+    echo "Difficulty: $thrd_difficulty%</p>"
+
+    echo "<p><a href='#'>⬆️</a></p>"
     echo "</div>"
+
+  done
+  echo "</div>"
+}
+
+kdr_bar() {
+  kills=$(jq ".[$match].kills.${1}" matches.json)
+  deaths=$(jq ".[$match].deaths.${1}" matches.json)
+  [ "$deaths" = 0 ] && deaths=1    # to avoid zero div
+  kdr=$(( kills * 100 / deaths ))
+
+  if [ "$kdr" -le 100 ]
+  then
+    percent=$(( kdr / 2 ))
+  else
+    percent=$(( 100 - 10000/(kdr*2) ))
+  fi
+
+  if [ "$percent" -gt 50 ]
+  then
+    lime=$percent && maroon=100
+  else
+    lime=0 && maroon=$(( percent*2 ))
+  fi
+
+  echo "
+    <div class='green' style='display:inline-block; border:solid black 2px; background-color: green; height: 10px; width: 100px;'>
+    <div class='lime' style='background-color: lime; width: ${lime}%; height: 100%;'>
+    <div class='red' style='background-color: red; width: 50px; height: 100%;'>
+    <div class='maroon' style='background-color: maroon; width: ${maroon}%; height: 100%;'>
+    </div>
+    </div>
+    </div>
+    </div> $kills kills / $deaths deaths ($(echo "scale=2; ($kdr/100)" | bc -l))
+  "
 }
 
 make_header() {
@@ -370,4 +338,4 @@ make_index \
 | sed s/:blue:/🔵/g \
 | sed s/:green:/🟢/g > index.html
 
-notify-send "index.html ready!" # OS alert
+# notify-send "index.html ready!" # OS alert
