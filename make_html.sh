a5752d0a4fa27fe822d9c0601c68a61f982f07d9
Michaël Dima <maniacmika@gmail.com>
2023-04-08 15:48:03 +0200

    Add files via upload

--- make_html.sh
+++ make_html.sh
@@ -2,19 +2,17 @@
 #
 # Create html page with matches information.
 
-GW2MATCHES="https://api.guildwars2.com/v2/wvw/matches"
-GW2WORLDS="https://api.guildwars2.com/v2/worlds"
+# functions
 
 dl_matches() {
+    GW2MATCHES="https://api.guildwars2.com/v2/wvw/matches"
     wget --quiet --output-document="matches.json" "$GW2MATCHES?ids=all"
 }
 
 dl_worlds() {
+    GW2WORLDS="https://api.guildwars2.com/v2/worlds"
     wget --quiet --output-document="worlds.json" "$GW2WORLDS?ids=all"
 }
-
-[ -f "worlds.json" ] || dl_worlds
-[ -f "matches.json" ] || dl_matches
 
 make_list_matches() {
     echo "<div id='matches'>"
@@ -27,16 +25,6 @@ make_list_matches() {
     done
     echo "</ul>"
     echo "</div>"
-}
-
-li_world() {
-    echo "<ul>"
-    for world_id in ${1}; do
-        world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
-        world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
-        echo "<li><a href='#$world_id'>$world_id :$world_pop: $world_name</a>"
-    done
-    echo "</ul>"
 }
 
 make_list_worlds() {
@@ -67,13 +55,30 @@ make_list_worlds() {
     echo "</div>"
 }
 
+li_world() {
+    echo "<ul>"
+    for world_id in ${1}; do
+        world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
+        world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
+        echo "<li><a href='#$world_id'>$world_id :$world_pop: $world_name</a>"
+#        echo "<li><a href='#$world_id'>$world_id $world_name</a>"
+    done
+    echo "</ul>"
+}
+
 make_match() {
   echo "<div id='results'>"
   echo "<h2>results</h2>"
   MATCHES=$(jq ".|keys[]" matches.json)
   for match in $MATCHES
   do
-    echo "<div class='match' style='margin-bottom: 300px;'>"
+    match_info
+  done
+  echo "</div>"
+}
+
+match_info() {
+    echo "<article class='match'>"
     match_id=$(jq -r ".[$match].id" matches.json)
     echo "<h3 id='$match_id'>$match_id</h3>"
 
@@ -84,11 +89,12 @@ make_match() {
     skirmish_done=$(( (vp_red+vp_blue+vp_green) / (3+4+5) ))
     skirmish_remaining=$((SKIRMISH_TOTAL - skirmish_done))
     vp_remaining=$((skirmish_remaining * 2))
-    echo "<p>Skirmish: $skirmish_done/$SKIRMISH_TOTAL ($skirmish_remaining left, max $vp_remaining VPd)</p>"
-
+    echo "<p>Skirmishes completed: $skirmish_done/$SKIRMISH_TOTAL<br>"
+    echo "Skirmishes left: $skirmish_remaining<br>"
+    echo "Max earnable VP difference: $vp_remaining</p>"
+    echo "<div class='rbg' style='display: flex; justify-content: space-between; align-items: flex-end;'>"
     vp_max=$((skirmish_done * 5))
     vp_min=$((skirmish_done * 3))
-
 
     if [ "$vp_red" -gt "$vp_blue" ] && [ "$vp_red" -gt "$vp_green" ]
     then
@@ -139,85 +145,76 @@ make_match() {
       fi
     fi
 
-
     [ $vp_max = $vp_min ] && frst_victory_ratio=0 \
     || frst_victory_ratio=$(( 100 * (frst - vp_min) / (vp_max - vp_min) ))
-
     frst_vp_difference=$((frst - scnd))
     frst_tie=$(( (vp_remaining - frst_vp_difference) / 2 ))
     frst_secure=$((frst_tie + 1))
     frst_difficulty=$((100 * frst_secure / vp_remaining))
-
-
+#    kdr_bar "$frst_color"
     echo "<p>"
     for world_id in $(jq ".[$match].all_worlds.${frst_color}[]" matches.json)
     do
       world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
       world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
-      echo "<span class='$frst_color' style='color:$frst_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
+      echo "<b class='$frst_color' id='$world_id'>:$world_pop: $world_name</b><br>"
     done
-    echo "</p>"
+    echo "Victory Points: $frst<br>"
+    echo "Victory Ratio: $frst_victory_ratio%<br>"
+    echo ":$frst_color:🥇 vs :$scnd_color:🥈: $frst_vp_difference<br>"
+    echo "Homestretch: $frst_tie<br>"
+    echo "Difficulty: $frst_difficulty% - $(( 100 - frst_difficulty ))%<br>"
+    echo "Certitude: $(( 2 * (50 - frst_difficulty) ))%<br>"
+    echo "Prediction: $(( frst+(skirmish_remaining*3)+(vp_remaining*frst_victory_ratio/100)+1 ))</p>"
 
-    kdr_bar "$frst_color"
-
-    echo "<p>:$frst_color: Victory Ratio: $frst_victory_ratio%<br>"
-    echo "$frst Victory Points<br>"
-    echo "VP difference with $scnd_color: $frst_vp_difference<br>"
-    echo "Will require +$frst_tie VP over $scnd_color for a tie, and one more to secure position<br>"
-    echo "Difficulty: $frst_difficulty%</p>"
-
-    [ $vp_max = $vp_min ] && scnd_victory_ratio=0 || scnd_victory_ratio=$(( 100 * (scnd - vp_min) / (vp_max - vp_min) ))
+    [ $vp_max = $vp_min ] && scnd_victory_ratio=0 \
+    || scnd_victory_ratio=$(( 100 * (scnd - vp_min) / (vp_max - vp_min) ))
     scnd_vp_difference=$((scnd - thrd))
     scnd_tie=$(( (vp_remaining - scnd_vp_difference) / 2 ))
     scnd_secure=$((scnd_tie + 1))
     scnd_difficulty=$((100 * scnd_secure / vp_remaining))
-
-
+#    kdr_bar "$scnd_color"
     echo "<p>"
     for world_id in $(jq ".[$match].all_worlds.${scnd_color}[]" matches.json)
     do
       world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
       world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
-      echo "<span class='$scnd_color' style='color:$scnd_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
+      echo "<b class='$scnd_color' id='$world_id'>:$world_pop: $world_name</b><br>"
     done
-    echo "</p>"
+    echo "Victory Points: $scnd<br>"
+    echo "Victory Ratio: $scnd_victory_ratio%<br>"
+    echo ":$scnd_color:🥈 vs :$thrd_color:🥉: $scnd_vp_difference<br>"
+    echo "Homestretch: $scnd_tie<br>"
+    echo "Difficulty: $scnd_difficulty% - $(( 100 - scnd_difficulty ))%<br>"
+    echo "Certitude: $(( 2 * (50 - scnd_difficulty) ))%<br>"
+    echo "Prediction: $(( scnd+(skirmish_remaining*3)+(vp_remaining*scnd_victory_ratio/100)+1 ))</p>"
 
-    kdr_bar "$scnd_color"
-
-    echo "<p>:$scnd_color: Victory Ratio: $scnd_victory_ratio%<br>"
-    echo "$scnd Victory Points<br>"
-    echo "VP difference with $thrd_color: $scnd_vp_difference<br>"
-    echo "Will require +$scnd_tie VP over opponent for a tie, and one more to secure position<br>"
-    echo "Difficulty: $scnd_difficulty%</p>"
-
-    [ $vp_max = $vp_min ] && thrd_victory_ratio=0 || thrd_victory_ratio=$(( 100 * (thrd - vp_min) / (vp_max - vp_min) ))
+    [ $vp_max = $vp_min ] && thrd_victory_ratio=0 \
+    || thrd_victory_ratio=$(( 100 * (thrd - vp_min) / (vp_max - vp_min) ))
     thrd_vp_difference=$((thrd - frst))
     thrd_tie=$(( (vp_remaining - thrd_vp_difference) / 2 ))
     thrd_secure=$((thrd_tie + 1))
     thrd_difficulty=$((100 * thrd_secure / vp_remaining))
-
+#    kdr_bar "$thrd_color"
     echo "<p>"
     for world_id in $(jq ".[$match].all_worlds.${thrd_color}[]" matches.json)
     do
       world_name=$(jq -r ".[]|select(.id==$world_id).name" worlds.json)
       world_pop=$(jq -r ".[]|select(.id==$world_id).population" worlds.json)
-      echo "<span class='$thrd_color' style='color:$thrd_color;' id='$world_id'>$world_id :$world_pop: $world_name</span><br>"
+      echo "<b class='$thrd_color' id='$world_id'>:$world_pop: $world_name</b><br>"
     done
-    echo "</p>"
+    echo "Victory Points: $thrd<br>"
+    echo "Victory Ratio: $thrd_victory_ratio%<br>"
+    echo ":$thrd_color:🥉 vs :$frst_color:🥇: $thrd_vp_difference<br>"
+    echo "Homestretch: $thrd_tie<br>"
+    echo "Difficulty: $thrd_difficulty% - $(( 100 - thrd_difficulty ))%<br>"
+    echo "Certitude: $(( 2 * (thrd_difficulty-50) ))%<br>"
+    echo "Prediction: $(( thrd+(skirmish_remaining*3)+(vp_remaining*thrd_victory_ratio/100)+1 ))</p>"
 
-    kdr_bar "$thrd_color"
-
-    echo "<p>:$thrd_color: Victory Ratio: $thrd_victory_ratio%<br>"
-    echo "$thrd Victory Points<br>"
-    echo "VP difference with $frst_color: $thrd_vp_difference<br>"
-    echo "Will require +$thrd_tie VP to catch up with $frst_color<br>"
-    echo "Difficulty: $thrd_difficulty%</p>"
-
-    echo "<p><a href='#'>⬆️</a></p>"
     echo "</div>"
+    echo "<p><a href='#'>⬆️Return to top</a></p>"
+    echo "</article>"
 
-  done
-  echo "</div>"
 }
 
 kdr_bar() {
@@ -241,94 +238,72 @@ kdr_bar() {
   fi
 
   echo "
-    <div class='green' style='display:inline-block; border:solid black 2px; background-color: green; height: 10px; width: 100px;'>
+    <span> $kills kills / $deaths deaths ($(echo "scale=2; ($kdr/100)" | bc -l))</span>
+    <div class='green' style='border:solid black 2px; background-color: green; height: 10px; width: 100px;'>
     <div class='lime' style='background-color: lime; width: ${lime}%; height: 100%;'>
     <div class='red' style='background-color: red; width: 50px; height: 100%;'>
     <div class='maroon' style='background-color: maroon; width: ${maroon}%; height: 100%;'>
     </div>
     </div>
     </div>
-    </div> $kills kills / $deaths deaths ($(echo "scale=2; ($kdr/100)" | bc -l))
+    </div>
   "
 }
 
-make_header() {
+make_index() {
     last_updated=$(date -Is -u)
     echo '<!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
 <title>gw2skirmish</title>
-    <style>
-      @media (prefers-color-scheme: light) {
-        .main {
-          background-color: white;
-          color: black;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-          font-size: 14px;
-        }
-        a:link {
-          color: #3498db;
-        }
-        a:visited {
-          color: #8e44ad;
-        }
-        a:hover {
-          color: #3498db;
-        }
-        a:active {
-          color: #8e44ad;
-        }
-      }
-      @media (prefers-color-scheme: dark) {
-        .main {
-          background-color: #222222;
-          color: #ddd;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
-          font-size: 14px;
-        }
-        a:link {
-          color: #b3b3de;
-        }
-        a:visited {
-          color: #bca9c8;
-        }
-        a:hover {
-          color: #b3b3de;
-        }
-        a:active {
-          color: #bca9c8;
-        }
-      }
-    </style>
+<link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+<style>
+.red {
+  color: #EE0000;
+}
+.blue {
+  color: #6161FF;
+}
+.green {
+  color: #008A00;
+}
+@media (prefers-color-scheme: dark) {
+  .red {
+    color: #FF7878;
+  }
+  .blue {
+    color: #7878FF;
+  }
+  .green {
+    color: #78FF78;
+  }
+}
+</style>
 </head>
-
 <body class="main">
+<header>
 <h1 id="#">gw2skirmish</h1>'
     echo "<p>Last updated: $last_updated</p>"
-}
-
-make_footer() {
+    echo "<p><a href='https://github.com/MikaMika/gw2skirmish'>Link to GitHub</a></p>"
+    echo "</header>"
+    echo "<nav>"
+    make_list_matches
+    make_list_worlds
+    echo "</nav>"
+    echo "<main>"
+    make_match
+    echo "</main>"
+    echo "<footer>"
+    echo "</footer>"
     echo '</body>
-
 </html>'
 }
 
-make_index() {
-    cat header.html \
-        list.html \
-        worlds.html \
-        match.html \
-        footer.html
-}
-
-#dl_worlds
-#dl_matches
-make_header > header.html
-make_list_matches > list.html
-make_list_worlds > worlds.html
-make_match > match.html
-make_footer > footer.html
+# exec
+start=`date +%s`
+[ -f "worlds.json" ] || dl_worlds
+[ -f "matches.json" ] || dl_matches
 make_index \
 | sed s/:Full:/🟥/g \
 | sed s/:VeryHigh:/🟧/g \
@@ -337,5 +312,8 @@ make_index \
 | sed s/:red:/🔴/g \
 | sed s/:blue:/🔵/g \
 | sed s/:green:/🟢/g > index.html
-
-# notify-send "index.html ready!" # OS alert
+#rm worlds.json
+#rm matches.json
+end=`date +%s`
+runtime=$((end-start))
+notify-send "index.html done in $runtime seconds"
